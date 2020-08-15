b32d655a0866f066f35bf2f5bcae2dffce9224b5
Myron Apostolakis <myron.apostolakis@sunlight.io>
2020-08-15 19:01:40 +0300

    read csv data and run model example

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "../css/css.styl";
+import * as utils from "../utils";
 
 const App = () => {
+  // Similar to componentDidMount and componentDidUpdate:
+  const { values, columnNames, numOfFeatures } = utils.loadData();
+  // useEffect(() => {
+  //   // utils.trainModel({ values, columnNames, numOfFeatures });
+  // }, []);
+
   return <div className="wrapper">I am a react component</div>;
 };
 
